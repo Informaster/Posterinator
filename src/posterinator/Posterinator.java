fa6a550a1885d0c319bf260fe7bb660c05591f45
Informaster <arthur.pabst@t-online.de>
2014-12-14 11:44:33 +0100

    1.

---
+++ src/posterinator/Posterinator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posterinator;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Posterinator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        window w=new window();
+        w.setSize(500, 500);
+        w.setVisible(true);
+    }
+    
+}
